796b91dc75a514d0c3da2761f4eb2c6243ea7533
dzsolic <dzejlan.solic.24@size.ba>
2025-12-25 20:45:11 +0100

    Logo and Splash screen

--- BHFanShop/BHFanShop/App.xaml.cs
+++ BHFanShop/BHFanShop/App.xaml.cs
@@ -2,16 +2,22 @@
 
 namespace BHFanShop
 {
+
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
+
+            MainPage = new AppShell();
+
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
             return new Window(new AppShell());
         }
+        
+
     }
 }
