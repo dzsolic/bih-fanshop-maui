db7e8aeded3afa39f9bbf5f2be46d63398785939
dzsolic <dzejlan.solic.24@size.ba>
2025-12-27 19:01:24 +0100

    Jersey Shop added

---
+++ BHFanShop/BHFanShop/Models/Jersey.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BHFanShop.Models;
+
+public class Jersey
+{
+    public string Name { get; set; }
+    public string Price { get; set; }
+    public string Image { get; set; }
+}
--- BHFanShop/BHFanShop/Views/ShopPage.xaml.cs
+++ BHFanShop/BHFanShop/Views/ShopPage.xaml.cs
@@ -1,9 +1,56 @@
+using BHFanShop.Models;
+
 namespace BHFanShop.Views;
 
 public partial class ShopPage : ContentPage
 {
-	public ShopPage()
-	{
-		InitializeComponent();
-	}
+    public ShopPage()
+    {
+        InitializeComponent();
+
+        JerseysCollection.ItemsSource = new List<Jersey>
+{
+    new Jersey
+    {
+        Name = "Domaæi dres",
+        Price = "109 KM",
+        Image = "dres_domaci.jpeg"
+    },
+    new Jersey
+    {
+        Name = "Gostujuæi dres",
+        Price = "109 KM",
+        Image = "dres_gostujuci.jpeg"
+    },
+    new Jersey
+    {
+        Name = "Retro dres",
+        Price = "129 KM",
+        Image = "retro.png"
+    }
+};
+
+    }
+
+    private async void OnBuyClicked(object sender, EventArgs e)
+    {
+        string phone = await DisplayPromptAsync(
+            "Kupovina",
+            "Unesite broj telefona:");
+
+        if (string.IsNullOrWhiteSpace(phone))
+            return;
+
+        string address = await DisplayPromptAsync(
+            "Kupovina",
+            "Unesite adresu:");
+
+        if (string.IsNullOrWhiteSpace(address))
+            return;
+
+        await DisplayAlert(
+            "Uspješno",
+            "Kupovina je uspješno izvršena!",
+            "OK");
+    }
 }
