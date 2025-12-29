8ea1bc5402096597ce35c0768a9da94601fefbe9
dzsolic <dzejlan.solic.24@size.ba>
2025-12-29 18:54:38 +0100

    Footballer's Images implemented

--- BHFanShop/BHFanShop/Services/PlayerData.cs
+++ BHFanShop/BHFanShop/Services/PlayerData.cs
@@ -10,106 +10,108 @@ namespace BHFanShop.Services
     {
         public static ObservableCollection<Player> ATeam = new ObservableCollection<Player>
         {
-            new Player { Name = "Nikola Vasilj", Club = "FC St. Pauli", Position = "Golman", Image="vasilj.jpeg" },
-            new Player { Name = "Martin Zlomislić", Club = "HNK Rijeka", Position = "Golman" },
-            new Player { Name = "Osman Hadžikić", Club = "Slaven Belupo", Position = "Golman" },
+            new Player { Name = "Nikola Vasilj", Club = "FC St. Pauli", Position = "Golman", Image = "vasilj.jpeg" },
+            new Player { Name = "Martin Zlomislić", Club = "HNK Rijeka", Position = "Golman", Image = "zlomslic.jpeg" },
+            new Player { Name = "Osman Hadžikić", Club = "Slaven Belupo", Position = "Golman", Image = "hadzikic.jpeg" },
 
-            new Player { Name = "Tarik Muharemović", Club = "US Sassuolo", Position = "Štoper" },
-            new Player { Name = "Sead Kolašinac", Club = "Atalanta BC", Position = "Štoper" },
-            new Player { Name = "Adrian Leon Barišić", Club = "FC Basel 1893", Position = "Štoper" },
-            new Player { Name = "Nikola Katić", Club = "FC Schalke 04", Position = "Štoper" },
-            new Player { Name = "Dennis Hadžikadunić", Club = "UC Sampdoria", Position = "Štoper" },
-            new Player { Name = "Nihad Mujakić", Club = "Eyüpspor", Position = "Štoper" },
+            new Player { Name = "Tarik Muharemović", Club = "US Sassuolo", Position = "Štoper", Image = "muharemovic.jpeg" },
+            new Player { Name = "Sead Kolašinac", Club = "Atalanta BC", Position = "Štoper", Image = "kolasinac.jpeg" },
+            new Player { Name = "Adrian Leon Barišić", Club = "FC Basel 1893", Position = "Štoper", Image = "barisic.jpeg" },
+            new Player { Name = "Nikola Katić", Club = "FC Schalke 04", Position = "Štoper", Image = "katic.jpeg" },
+            new Player { Name = "Dennis Hadžikadunić", Club = "UC Sampdoria", Position = "Štoper", Image = "hadzikadunic.jpeg" },
+            new Player { Name = "Nihad Mujakić", Club = "Eyüpspor", Position = "Štoper", Image = "mujakic.jpeg" },
 
-            new Player { Name = "Emir Karić", Club = "SK Sturm Graz", Position = "Lijevi bek" },
-            new Player { Name = "Amar Dedić", Club = "SL Benfica", Position = "Desni bek" },
-            new Player { Name = "Arjan Malić", Club = "SK Sturm Graz", Position = "Desni bek" },
+            new Player { Name = "Emir Karić", Club = "SK Sturm Graz", Position = "Lijevi bek", Image = "karic.jpeg" },
+            new Player { Name = "Amar Dedić", Club = "SL Benfica", Position = "Desni bek", Image = "dedic.jpeg" },
+            new Player { Name = "Arjan Malić", Club = "SK Sturm Graz", Position = "Desni bek", Image = "malic.jpeg" },
 
-            new Player { Name = "Amir Hadžiahmetović", Club = "Hull City", Position = "Zadnji vezni" },
-            new Player { Name = "Ivan Šunjić", Club = "Pafos FC", Position = "Zadnji vezni" },
-            new Player { Name = "Dženis Burnić", Club = "Karlsruher SC", Position = "Zadnji vezni" },
+            new Player { Name = "Amir Hadžiahmetović", Club = "Hull City", Position = "Zadnji vezni", Image = "hadziahmetovic.jpeg" },
+            new Player { Name = "Ivan Šunjić", Club = "Pafos FC", Position = "Zadnji vezni", Image = "sunjic.jpeg" },
+            new Player { Name = "Dženis Burnić", Club = "Karlsruher SC", Position = "Zadnji vezni", Image = "burnic.jpeg" },
 
-            new Player { Name = "Benjamin Tahirović", Club = "Bröndby IF", Position = "Vezni" },
-            new Player { Name = "Armin Gigović", Club = "BSC Young Boys", Position = "Vezni" },
-            new Player { Name = "Ivan Bašić", Club = "FC Astana", Position = "Vezni" },
-            new Player { Name = "Dario Šarić", Club = "Antalyaspor", Position = "Vezni" },
+            new Player { Name = "Benjamin Tahirović", Club = "Bröndby IF", Position = "Vezni", Image = "tahirovic.jpeg" },
+            new Player { Name = "Armin Gigović", Club = "BSC Young Boys", Position = "Vezni", Image = "gigovic.jpeg" },
+            new Player { Name = "Ivan Bašić", Club = "FC Astana", Position = "Vezni", Image = "basic.jpeg" },
+            new Player { Name = "Dario Šarić", Club = "Antalyaspor", Position = "Vezni", Image = "saric.jpeg" },
 
-            new Player { Name = "Amar Memić", Club = "FC Viktoria Plzen", Position = "Desno krilo" },
+            new Player { Name = "Amar Memić", Club = "FC Viktoria Plzen", Position = "Desno krilo", Image = "memic.jpeg" },
 
-            new Player { Name = "Kerim Alajbegović", Club = "Red Bull Salzburg", Position = "Lijevo krilo" },
-            new Player { Name = "Esmir Bajraktarević", Club = "PSV Eindhoven", Position = "Desno krilo" },
+            new Player { Name = "Kerim Alajbegović", Club = "Red Bull Salzburg", Position = "Lijevo krilo", Image = "alajbegovic.jpeg" },
+            new Player { Name = "Esmir Bajraktarević", Club = "PSV Eindhoven", Position = "Desno krilo", Image = "bajraktarevic.jpeg" },
 
-            new Player { Name = "Haris Tabaković", Club = "Borussia Mönchengladbach", Position = "Napadač" },
-            new Player { Name = "Samed Baždar", Club = "Real Zaragoza", Position = "Napadač" },
-            new Player { Name = "Edin Džeko", Club = "ACF Fiorentina", Position = "Napadač" }
+            new Player { Name = "Haris Tabaković", Club = "Borussia Mönchengladbach", Position = "Napadač", Image = "tabakovic.jpeg" },
+            new Player { Name = "Samed Baždar", Club = "Real Zaragoza", Position = "Napadač", Image = "bazdar.jpeg" },
+            new Player { Name = "Edin Džeko", Club = "ACF Fiorentina", Position = "Napadač", Image = "dzeko.jpeg" }
+
 
         };
         public static ObservableCollection<Player> U21Team = new ObservableCollection<Player>
         {
-            new Player { Name = "Mladen Jurkaš", Club = "FK Borac Banja Luka", Position = "Golman" },
-            new Player { Name = "Tarik Karić", Club = "FK Velež Mostar", Position = "Golman" },
-            new Player { Name = "Sanin Mušija", Club = "FK Sarajevo", Position = "Golman" },
+            new Player { Name = "Mladen Jurkaš", Club = "FK Borac Banja Luka", Position = "Golman", Image = "notfound.jpeg" },
+            new Player { Name = "Tarik Karić", Club = "FK Velež Mostar", Position = "Golman", Image = "notfound.jpeg" },
+            new Player { Name = "Sanin Mušija", Club = "FK Sarajevo", Position = "Golman", Image = "musija.jpeg" },
 
-            new Player { Name = "Nidal Čelik", Club = "RC Lens", Position = "Štoper" },
-            new Player { Name = "Anes Čardaklija", Club = "GAIS", Position = "Štoper" },
-            new Player { Name = "Nermin Mujkić", Club = "FK Sarajevo", Position = "Štoper" },
-            new Player { Name = "Luka Mikulić", Club = "HŠK Posušje", Position = "Štoper" },
-            new Player { Name = "Haris Berbić", Club = "FK Borac Banja Luka", Position = "Štoper" },
-            new Player { Name = "Amar Ibrisimović", Club = "FC Schalke 04 U19", Position = "Štoper" },
+            new Player { Name = "Nidal Čelik", Club = "RC Lens", Position = "Štoper", Image = "celik.jpeg" },
+            new Player { Name = "Anes Čardaklija", Club = "GAIS", Position = "Štoper", Image = "cardaklija.jpeg" },
+            new Player { Name = "Nermin Mujkić", Club = "FK Sarajevo", Position = "Štoper", Image = "mujkic.jpeg" },
+            new Player { Name = "Luka Mikulić", Club = "HŠK Posušje", Position = "Štoper", Image = "mikulic.jpeg" },
+            new Player { Name = "Haris Berbić", Club = "FK Borac Banja Luka", Position = "Štoper", Image = "notfound.jpeg" },
+            new Player { Name = "Amar Ibrisimović", Club = "FC Schalke 04 U19", Position = "Štoper", Image = "ibrisimovic.jpeg" },
 
-            new Player { Name = "Filip Taraba", Club = "NK Istra 1961", Position = "Desni bek" },
-            new Player { Name = "Senad Mustafić", Club = "SK Sturm Graz II", Position = "Desni bek" },
+            new Player { Name = "Filip Taraba", Club = "NK Istra 1961", Position = "Desni bek", Image = "notfound.jpeg" },
+            new Player { Name = "Senad Mustafić", Club = "SK Sturm Graz II", Position = "Desni bek", Image = "mustafic.jpeg" },
 
-            new Player { Name = "Albin Omić", Club = "FK Loznica", Position = "Zadnji vezni" },
+            new Player { Name = "Albin Omić", Club = "FK Loznica", Position = "Zadnji vezni", Image = "omic.jpeg" },
 
-            new Player { Name = "Muhamed Buljubašić", Club = "Çaykur Rizespor", Position = "Vezni" },
-            new Player { Name = "Niko Kovač", Club = "US Lecce", Position = "Vezni" },
-            new Player { Name = "Emin Kujović", Club = "1. FC Köln", Position = "Vezni" },
+            new Player { Name = "Muhamed Buljubašić", Club = "Caykur Rizespor", Position = "Vezni", Image = "buljubasic.jpeg" },
+            new Player { Name = "Niko Kovač", Club = "US Lecce", Position = "Vezni", Image = "notfound.jpeg" },
+            new Player { Name = "Emin Kujović", Club = "1. FC Köln", Position = "Vezni", Image = "kujovic.jpeg" },
 
-            new Player { Name = "Nedim Keranović", Club = "FK Rudar Prijedor", Position = "Ofanzivni vezni" },
+            new Player { Name = "Nedim Keranović", Club = "FK Rudar Prijedor", Position = "Ofanzivni vezni", Image = "keranovic.jpeg" },
 
-            new Player { Name = "Aldin Mešić", Club = "FK Velež Mostar", Position = "Lijevo krilo" },
-            new Player { Name = "Amar Milak", Club = "FK Velež Mostar", Position = "Lijevo krilo" },
-            new Player { Name = "Benjamin Džanović", Club = "Eintracht Frankfurt U19", Position = "Lijevo krilo" },
+            new Player { Name = "Aldin Mešić", Club = "FK Velež Mostar", Position = "Lijevo krilo", Image = "mesic.jpeg" },
+            new Player { Name = "Amar Milak", Club = "FK Velež Mostar", Position = "Lijevo krilo", Image = "notfound.jpeg" },
+            new Player { Name = "Benjamin Džanović", Club = "Eintracht Frankfurt U19", Position = "Lijevo krilo", Image = "dzanovic.jpeg" },
 
-            new Player { Name = "Matej Šakota", Club = "HŠK Zrinjski Mostar", Position = "Desno krilo" },
-            new Player { Name = "Borna Filipović", Club = "HŠK Zrinjski Mostar", Position = "Desno krilo" },
+            new Player { Name = "Matej Šakota", Club = "HŠK Zrinjski Mostar", Position = "Desno krilo", Image = "sakota.jpeg" },
+            new Player { Name = "Borna Filipović", Club = "HŠK Zrinjski Mostar", Position = "Desno krilo", Image = "notfound.jpeg" },
 
-            new Player { Name = "Aleksandar Kahvić", Club = "NK Domžale", Position = "Napadač" },
-            new Player { Name = "Hamza Jaganjac", Club = "FK Željezničar Sarajevo", Position = "Napadač" },
-            new Player { Name = "Stefan Marčetić", Club = "FK Borac Banja Luka", Position = "Napadač" }
+            new Player { Name = "Aleksandar Kahvić", Club = "NK Domžale", Position = "Napadač", Image = "kahvic.jpeg" },
+            new Player { Name = "Hamza Jaganjac", Club = "FK Željezničar Sarajevo", Position = "Napadač", Image = "jaganjac.jpeg" },
+            new Player { Name = "Stefan Marčetić", Club = "FK Borac Banja Luka", Position = "Napadač", Image = "notfound.jpeg" }
+
 
         };
         public static ObservableCollection<Player> WomenTeam = new ObservableCollection<Player>
         {
-            new Player { Name = "Almina Hodžić", Club = "Fatih Vatan Spor", Position = "Golman" },
-            new Player { Name = "Iman Dumanjić", Club = "Giresun", Position = "Golman" },
-            new Player { Name = "Somea Položen", Club = "Sparta Prag", Position = "Golman" },
+            new Player { Name = "Almina Hodžić", Club = "Fatih Vatan Spor", Position = "Golman", Image = "notfound.jpeg"},
+            new Player { Name = "Iman Dumanjić", Club = "Giresun", Position = "Golman" , Image = "notfound.jpeg"},
+            new Player { Name = "Somea Položen", Club = "Sparta Prag", Position = "Golman" , Image = "notfound.jpeg"},
 
-            new Player { Name = "Nina Brnić", Club = "Hajduk Split", Position = "Odbrana" },
-            new Player { Name = "Samra Muhić", Club = "Arna Bjørnar", Position = "Odbrana" },
-            new Player { Name = "Emma Veletanlić", Club = "Kolding IF", Position = "Odbrana" },
-            new Player { Name = "Melisa Hasanbegović", Club = "Al-Ula", Position = "Odbrana" },
-            new Player { Name = "Đula Velagić", Club = "SFK 2000", Position = "Odbrana" },
-            new Player { Name = "Gloria Slišković", Club = "Napoli", Position = "Odbrana" },
-            new Player { Name = "Ena Šabanagić", Club = "Panathinaikos", Position = "Odbrana" },
-            new Player { Name = "Lana Radulović", Club = "Spartak", Position = "Odbrana" },
-            new Player { Name = "Aldina Hamzić", Club = "RB Salzburg", Position = "Odbrana" },
-            new Player { Name = "Marija Milinković", Club = "Inter Milan", Position = "Odbrana" },
-            new Player { Name = "Nikolina Milović", Club = "Giresun", Position = "Odbrana" },
-            new Player { Name = "Vina Crnoja", Club = "RB Salzburg", Position = "Odbrana" },
-            new Player { Name = "Andrea Ćule", Club = "Emina Mostar", Position = "Odbrana" },
-            new Player { Name = "Andrea Grebenar", Club = "ŽNK Hajduk", Position = "Odbrana" },
+            new Player { Name = "Nina Brnić", Club = "Hajduk Split", Position = "Odbrana" , Image = "notfound.jpeg"},
+            new Player { Name = "Samra Muhić", Club = "Arna Bjørnar", Position = "Odbrana", Image = "notfound.jpeg" },
+            new Player { Name = "Emma Veletanlić", Club = "Kolding IF", Position = "Odbrana", Image = "notfound.jpeg" },
+            new Player { Name = "Melisa Hasanbegović", Club = "Al-Ula", Position = "Odbrana" , Image = "notfound.jpeg"},
+            new Player { Name = "Đula Velagić", Club = "SFK 2000", Position = "Odbrana", Image = "notfound.jpeg" },
+            new Player { Name = "Gloria Slišković", Club = "Napoli", Position = "Odbrana", Image = "notfound.jpeg" },
+            new Player { Name = "Ena Šabanagić", Club = "Panathinaikos", Position = "Odbrana", Image = "notfound.jpeg" },
+            new Player { Name = "Lana Radulović", Club = "Spartak", Position = "Odbrana", Image = "notfound.jpeg" },
+            new Player { Name = "Aldina Hamzić", Club = "RB Salzburg", Position = "Odbrana" , Image = "notfound.jpeg"},
+            new Player { Name = "Marija Milinković", Club = "Inter Milan", Position = "Odbrana" , Image = "notfound.jpeg"},
+            new Player { Name = "Nikolina Milović", Club = "Giresun", Position = "Odbrana", Image = "notfound.jpeg" },
+            new Player { Name = "Vina Crnoja", Club = "RB Salzburg", Position = "Odbrana" , Image = "notfound.jpeg"},
+            new Player { Name = "Andrea Ćule", Club = "Emina Mostar", Position = "Odbrana" , Image = "notfound.jpeg"},
+            new Player { Name = "Andrea Grebenar", Club = "ŽNK Hajduk", Position = "Odbrana" , Image = "notfound.jpeg"},
 
-            new Player { Name = "Elma Husić", Club = "Emina Mostar", Position = "Vezni" },
-            new Player { Name = "Nina Garibija", Club = "IF Djurgården", Position = "Vezni" },
-            new Player { Name = "Milica Denda", Club = "Emina Mostar", Position = "Vezni" },
-            new Player { Name = "Marija Aleksić", Club = "Beşiktaş", Position = "Vezni" },
-            new Player { Name = "Ema Paljević", Club = "Nantes", Position = "Vezni" },
-            new Player { Name = "Minela Gačanica", Club = "PAOK", Position = "Vezni" },
+            new Player { Name = "Elma Husić", Club = "Emina Mostar", Position = "Vezni", Image = "notfound.jpeg" },
+            new Player { Name = "Nina Garibija", Club = "IF Djurgården", Position = "Vezni" , Image = "notfound.jpeg"},
+            new Player { Name = "Milica Denda", Club = "Emina Mostar", Position = "Vezni", Image = "notfound.jpeg" },
+            new Player { Name = "Marija Aleksić", Club = "Beşiktaş", Position = "Vezni", Image = "notfound.jpeg" },
+            new Player { Name = "Ema Paljević", Club = "Nantes", Position = "Vezni", Image = "notfound.jpeg" },
+            new Player { Name = "Minela Gačanica", Club = "PAOK", Position = "Vezni", Image = "notfound.jpeg" },
 
-            new Player { Name = "Sofija Krajšumović", Club = "CSKA Moscow", Position = "Napadač" },
-            new Player { Name = "Milena Nikolić", Club = "Basel", Position = "Napadač" }
+            new Player { Name = "Sofija Krajšumović", Club = "CSKA Moscow", Position = "Napadač", Image = "notfound.jpeg" },
+            new Player { Name = "Milena Nikolić", Club = "Basel", Position = "Napadač", Image = "notfound.jpeg" }
 
         };
     }
